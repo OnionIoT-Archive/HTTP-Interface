8c7aa218a210328d1684804d049217ebe390c86c
Zheng Han <zh@onion.io>
2014-03-27 14:18:09 -0400

    moved to nginx server

--- http_interface.py
+++ http_interface.py
@@ -1,8 +1,11 @@
 import amqp_rpc as rpc
 import json
 from flask import Flask, request, make_response
-from OpenSSL import SSL
-from flask_sslify import SSLify
+#from OpenSSL import SSL
+#from flask_sslify import SSLify
+import logging
+logging.basicConfig(filename='/var/log/onion.log',level=logging.DEBUG)
+
 
 app = Flask(__name__)
 
@@ -10,9 +13,9 @@ app = Flask(__name__)
 with open('/etc/onionConfig.json') as f:
     config = json.load(f)['API_SERVER']
 
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file(config['SSL_KEY'])
-context.use_certificate_file(config['SSL_CRT'])
+#context = SSL.Context(SSL.SSLv23_METHOD)
+#context.use_privatekey_file(config['SSL_KEY'])
+#context.use_certificate_file(config['SSL_CRT'])
 
 def callRemoteFunction(params):
     payload = {}
@@ -61,6 +64,8 @@ def callRemoteFunctionV2(params):
     if result != None:
         fid = result["functionId"]
         if params['verb'].upper() == "POST":
+            logging.debug(result['postParams'])
+            logging.debug(params['postParams'])
             for p in result['postParams']:
                 temp = str(params['postParams'][p])
                 postParams.append(temp)
@@ -81,62 +86,69 @@ def callRemoteFunctionV2(params):
 
 @app.route('/<version>/devices/<deviceId>/<path:path>', methods=['GET', 'POST', 'OPTIONS'])
 def onApiCall(version='v1', deviceId=None, path=None):
-    if deviceId == None:
-        return {"error":"invalid device id"};
+    try:
+        logging.debug("on request")
+        if deviceId == None:
+            return make_response(json.dumps({"error":"invalid device id"}));
 
-    data = {}
-    data['deviceId'] = deviceId
-    data["path"] = '/'+path
-    data["verb"] = request.method
+        data = {}
+        data['deviceId'] = deviceId
+        data["path"] = '/'+path
+        data["verb"] = request.method
 
 
-    print request.headers['Content-Type']
+        print request.headers['Content-Type']
 
-    if request.method == 'OPTIONS': 
-        print "on options"
-        response = make_response()
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
-        return response
+        if request.method == 'OPTIONS': 
+            print "on options"
+            response = make_response()
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+            return response
 
-    elif request.method == 'GET': 
-        pass
-    
-    elif request.method == 'POST': 
-        postParams = {}
-        if "form-data" in request.headers['Content-Type']:
-            for key in request.form:
-                postParams[key] = request.form[key]
+        elif request.method == 'GET': 
+            pass
+        
+        elif request.method == 'POST': 
+            postParams = {}
+            if "form" in request.headers['Content-Type']:
+                for key in request.form:
+                    postParams[key] = request.form[key]
 
-        elif "application/json" in request.headers['Content-Type']:
-            postParams = request.data
-        else:
-            try:
-                postParams = json.loads(request.data)
-            except Exception as e:
-                response = make_response(json.dumps({"error": str(e)}))
-                response.headers['Content-Type'] = 'application/json'
-                response.headers['Access-Control-Allow-Origin'] = '*'
-                return response
+            elif "application/json" in request.headers['Content-Type']:
+                postParams = request.data
+            else:
+                try:
+                    postParams = json.loads(request.data)
+                except Exception as e:
+                    logging.error(str(e))
+                    response = make_response(json.dumps({"error": str(e)}))
+                    response.headers['Content-Type'] = 'application/json'
+                    response.headers['Access-Control-Allow-Origin'] = '*'
+                    return response
   
-        data["postParams"] = postParams
+            data["postParams"] = postParams
 
-    if version == "v1":
-        result =  callRemoteFunction(data)
-    elif version == "v2":
-        result =  callRemoteFunctionV2(data)
-    else:
-        result = "API version not supported"
+        if version == "v1":
+            result =  callRemoteFunction(data)
+        elif version == "v2":
+            result =  callRemoteFunctionV2(data)
+        else:
+            result = "API version not supported"
+    except Exception as e:
+        result = str()
 
     result = {'error': result}
 
     response = make_response(json.dumps(result))
     response.headers['Content-Type'] = 'application/json'
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
     return response
 
 if __name__ == "__main__":
-    sslify = SSLify(app, subdomains=True)
-    sslify.app.run(host=config['SERVER_HOST'], port=config['SERVER_PORT'], debug=config['DEBUG_MODE'], ssl_context=context)
+    #sslify = SSLify(app, subdomains=True)
+    #sslify.app.run(host=config['SERVER_HOST'], port=config['SERVER_PORT'], debug=config['DEBUG_MODE'], ssl_context=context)
+    app.run(host=config['SERVER_HOST'], port=config['SERVER_PORT'], debug=config['DEBUG_MODE'])
     print "started..."
